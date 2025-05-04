9c0c4da3e86eb7dfee3e2b4dd7ea35f9732d19d6
Nirajkumar11 <neerajprajapati761@gmail.com>
2025-05-05 00:17:33 +0530

    Commit:- First Commit

---
+++ dear_guruji_education_app/lib/resourse/routes/app_routes.dart
@@ -0,0 +1,13 @@
+import 'package:dear_guruji_education_app/resourse/routes/routes_name.dart';
+import 'package:get/get.dart';
+
+class AppRoutes{
+  static appRoutes()=>[
+    /*GetPage(name: RoutesName.splashScreen,
+        page: ()=>SplashScreen(),
+        transitionDuration: Duration(microseconds: 180),
+        transition: Transition.rightToLeftWithFade
+    ),*/
+
+  ];
+}
---
+++ dear_guruji_education_app/lib/resourse/routes/routes_name.dart
@@ -0,0 +1,6 @@
+class RoutesName{
+  //this is initial route and don't need to give name and we rome from main where home ->SplashPage()
+  static const splashScreen="/";
+
+
+}
