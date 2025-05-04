be74f0df12baeffaec21bb58a9f52bc0d67ec14a
Nirajkumar11 <neerajprajapati761@gmail.com>
2025-05-05 00:49:18 +0530

    Commit:- First Commit

--- dear_guruji_education_app/lib/resourse/routes/app_routes.dart
+++ dear_guruji_education_app/lib/resourse/routes/app_routes.dart
@@ -1,13 +1,15 @@
 import 'package:dear_guruji_education_app/resourse/routes/routes_name.dart';
 import 'package:get/get.dart';
 
+import '../../view/splash_view/splash_screen.dart';
+
 class AppRoutes{
   static appRoutes()=>[
-    /*GetPage(name: RoutesName.splashScreen,
+    GetPage(name: RoutesName.splashScreen,
         page: ()=>SplashScreen(),
         transitionDuration: Duration(microseconds: 180),
         transition: Transition.rightToLeftWithFade
-    ),*/
+    ),
 
   ];
 }
